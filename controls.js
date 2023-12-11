496b4775023b7946bcda47fd8d3c81877c854d82
ju-jmdr <153402341+ju-jmdr@users.noreply.github.com>
2023-12-11 17:28:51 -0300

    Update controls.js
    Adicionando a ação de agachar

--- controls.js
+++ controls.js
@@ -10,6 +10,8 @@ export default
     jump: new KeyboardEvent('Keydown', {key: 'Space', keyCode: 32}),
 
     //AGACHAR
+    crounch: new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40}),
+    
     dispatch(event)
     {
         document.dispatchEvent(this[event]);
