289924b654288b9c2fa47a2e5dc076952e94b2fa
ju-jmdr <153402341+ju-jmdr@users.noreply.github.com>
2023-12-11 17:30:55 -0300

    Update utils.js
    Ajuste na função matemática

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 export default {
-    getDistance(obj1, obj2) // ->Paramêtros do método
     {
-        return Math.sqrt(Math.abs(obj1.x - obj2.x) ** 2 + Math.abs(obj1.y - obj2.y ** 2));
-    }
+        return Math.sqrt(Math.abs(obj1.x - obj2.x) ** 2 + Math.abs(obj1.y - obj2.y ) ** 2);
+    },
 }
